4e865a581baac5812a0047c1a8950f610ef9586a
Em01 <emma_williams1@hotmail.co.uk>
2015-03-27 12:36:57 +0000

    added some pods

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AngleGradientLayer
+#define COCOAPODS_POD_AVAILABLE_AngleGradientLayer
+#define COCOAPODS_VERSION_MAJOR_AngleGradientLayer 1
+#define COCOAPODS_VERSION_MINOR_AngleGradientLayer 2
+#define COCOAPODS_VERSION_PATCH_AngleGradientLayer 1
+
+// SAMGradientView
+#define COCOAPODS_POD_AVAILABLE_SAMGradientView
+#define COCOAPODS_VERSION_MAJOR_SAMGradientView 0
+#define COCOAPODS_VERSION_MINOR_SAMGradientView 2
+#define COCOAPODS_VERSION_PATCH_SAMGradientView 1
+
